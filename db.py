64061da755a239f09c42369d83dec30476c9f2bf
Lezor01 <ellezor0204@gmail.com>
2025-11-10 20:47:29 -0600

    Add files via upload

--- db.py
+++ db.py
@@ -1,10 +1,18 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+# Cargar las variables del archivo .env
+load_dotenv()
 
 def get_db():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="dexter",
-        database="maki_roll_samr",
-        port=3306
+    """Crea y devuelve una conexión a la base de datos MySQL."""
+    conn = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),
+        ssl_disabled=True
     )
+    return conn
